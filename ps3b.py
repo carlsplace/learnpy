aa3b6d35f5b7b9643f28bc494e8f1e73ba4a76f0
Cal <chang.1123@outlook.com>
2016-03-31 21:46:24 +0800

    ps3b done

--- ps3b.py
+++ ps3b.py
@@ -1,2 +1,22 @@
 def subStringMatchExact(target,key):
+    pos = []
+    for i in range(0, len(target)):
+        if target[i:i+len(key)] == key:
+            pos.append(i)
+    return tuple(pos)
     
+target1 = 'atgacatgcacaagtatgcat'
+target2 = 'atgaatgcatggatgtaaatgcag'
+key10 = 'a'
+key11 = 'atg'
+key12 = 'atgc'
+key13 = 'atgca' 
+# print(len(target1), len(target2))
+print(subStringMatchExact(target1, key10))
+print(subStringMatchExact(target1, key11))
+print(subStringMatchExact(target1, key12))
+print(subStringMatchExact(target1, key13))
+print(subStringMatchExact(target2, key10))
+print(subStringMatchExact(target2, key11))
+print(subStringMatchExact(target2, key12))
+print(subStringMatchExact(target2, key13))
