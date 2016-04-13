4dd27f6d082479b5b90c5c1e4bbfef8db1e2df19
Cal <chang.1123@outlook.com>
2016-04-13 22:30:49 +0800

    debug recursion.py

--- recursion.py
+++ recursion.py
@@ -1,28 +1,33 @@
-
 def isPalindrome(word):
-	if len(word) <= 1:
-		print("isPalindrome")
-		return True
-	elif word[0] == word[-1]:
-		isPalindrome(word[1:-1])
-	else:
-		print("notPalindrome")
-		return False
+    if len(word) <= 1:
+        print("isPalindrome")
+        print(word)
+        return True
+    elif word[0] == word[-1]:
+        isPalindrome(word[1:-1])
+        print("test")
+    else:
+        print("notPalindrome")
+        return False
+# 'a' True
+# 'aa' None
+
+
 
 # def isPalindrome(s):
 #     """Return True if s is a palindrome and False otherwise"""
 #     if len(s) <=1:
-#     		return True
+#             return True
 #     else:
-#     		return s[0] == s[-1] and isPalindrome(s[1:-1])
+#             return s[0] == s[-1] and isPalindrome(s[1:-1])
 
-print(isPalindrome('aabaa'))
+print(isPalindrome('bab'))
 
 
 def fib(n):
-	if n == 1 or n == 2:
-		return 1
-	else:
-		return fib(n-1) + fib(n-2)
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
 
 print(fib(24))
